47a1fe63ee627cb98a54fd1f9e3eea65c38c91ec
DzenitaAdzic <dzenita_adzic@hotmail.com>
2015-06-02 23:35:51 +0200

    2.6.2015<Uploaded actor.cpp

---
+++ src/actor.cpp
@@ -0,0 +1,93 @@
+#include "actor.h"
+
+
+Actor::Actor(std::string firstName,std::string lastName,std::string Birthdate,std::string Birthplace){
+	_FirstName=firstName;
+	_LastName=lastName;
+	_BirthDate=Birthdate;
+	_BirthPlace=Birthplace;
+	}
+	
+Actor::Actor(std::string firstName,std::string lastName){
+	_FirstName=firstName;
+	_LastName=lastName;
+	
+	}
+	
+int Actor::getID() const { return _ID; }
+std::string Actor::getFirstName() const { return _FirstName;}
+std::string Actor::getLastName() const { return _LastName;}
+std::string Actor::getBirthDate() const { return _BirthDate;}
+std::string Actor::getBirthPlace() const { return _BirthPlace;}
+
+void Actor::setID(int ID) {
+	_ID=ID;
+	}
+void Actor::setFirstName(std::string fName){
+	_FirstName=fName;
+	}
+void Actor::setLastName(std::string lName){
+	_LastName=lName;
+	}
+void Actor::setBirthDate(std::string Bdate){
+	_BirthDate=Bdate;
+	}
+void Actor::setBirthPlace(std::string Bplace){
+	_BirthPlace=Bplace;
+	}
+
+void Actor::setActor(std::string firstName,std::string lastName,std::string Birthdate,std::string Birthplace){
+	_FirstName=firstName;
+	_LastName=lastName;
+	_BirthDate=Birthdate;
+	_BirthPlace=Birthplace;
+
+}
+void Actor::setActor(std::string firstName,std::string lastName){
+	_FirstName=firstName;
+	_LastName=lastName;
+}
+
+bool Actor::operator<(const Actor& other) const {
+	return _ID<other._ID;
+	}
+bool Actor::operator<=(const Actor& other)const{
+	return _ID<=other._ID;
+	}
+bool Actor::operator>(const Actor& other)const {
+	return _ID>other._ID;
+	}
+bool Actor::operator>=(const Actor& other)const{
+	return _ID>=other._ID;
+	}
+bool Actor::operator==(const Actor& other)const{
+	return _ID==other._ID;
+	}
+bool Actor::operator!=(const Actor& other) const{
+	return _ID!=other._ID;
+	}
+
+std::ostream& operator<<(std::ostream& out,const Actor& actor) {
+	out<<actor._FirstName<<" "<<actor._LastName<<" "<<actor._BirthDate<<" "<<actor._BirthPlace<<std::endl;
+	return out;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
