8c8fa9261f5a65c0c3d63c43be65505b709b0224
MenisaHrvic <menisa.hrvic@hotmail.com>
2015-06-02 19:03:26 +0200

    2.6.2015.<User record class initial commit>

---
+++ include/user.h
@@ -0,0 +1,49 @@
+#ifndef _USER_H
+#define _USER_H
+#include <string>
+#include <iostream>
+
+class User
+{
+	private: 
+		int _ID;
+		std::string _first_name;
+		std::string _last_name;
+		std::string _adress;
+		std::string _phone;
+		friend std::ostream& operator<<(std::ostream& out,const User& user);
+
+	public:
+		User() = default;
+		User(int ID,const std::string& first_name,const std::string& last_name, 
+				const std::string& adress,const std::string& phone);
+				
+		int getID() const;
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getAdress() const;
+		std::string getPhone() const;
+		
+		void setUser(int ID,const std::string& first_name, const std::string& last_name,
+						const std::string& adress,const std::string& phone);
+		void setID(int ID);
+		void setFirstName(std::string first_name);	
+		void setLastName(std::string last_name);
+		void setAdress(std::string adress);
+		void setPhone(std::string phone);
+		
+		bool operator<(User& other);
+		bool operator<=(User& other);
+		bool operator>(User& other);
+		bool operator>=(User& other);
+		bool operator==(User& other);
+		bool operator!=(User& other);
+		
+		bool byName(User& other);
+		bool bySurname(User& other);
+		bool byNameAndSurname(User& other);
+};
+
+
+
+#endif
