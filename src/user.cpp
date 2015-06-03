6469dcc984bdb6bb16f0099f674fafbe36b8e211
MenisaHrvic <menisa.hrvic@hotmail.com>
2015-06-03 10:37:54 +0200

    3.6.2015.<Updated user.cpp>

--- src/user.cpp
+++ src/user.cpp
@@ -1,4 +1,5 @@
-#include "user.h"
+#include <iostream>
+#include "../include/user.h"
 
 User::User(int ID,const std::string& first_name,const std::string& last_name, 
 			const std::string& phone,const std::string& adress): 
@@ -10,11 +11,11 @@ User::User(const std::string& first_name,const std::string& last_name,
 			const std::string& phone,const std::string& adress): 
             _first_name(first_name),_last_name(last_name),_phone(phone), _adress(adress) {}
             
-int User::getID() const {return _ID;}            
-std::string User::getFirstName() const {return _first_name;}
-std::string User::getLastName() const {return _last_name;}
-std::string User::getAdress() const {return _adress;}
-std::string User::getPhone() const {return _phone;}
+const int& User::getID() const {return _ID;}            
+const std::string& User::getFirstName() const {return _first_name;}
+const std::string& User::getLastName() const {return _last_name;}
+const std::string& User::getAdress() const {return _adress;}
+const std::string& User::getPhone() const {return _phone;}
 
 void User::setUser(int ID,const std::string& first_name, const std::string& last_name,
 						const std::string& adress, const std::string& phone) 
@@ -27,28 +28,31 @@ void User::setUser(int ID,const std::string& first_name, const std::string& last
 }
 
 void User::setID(int ID) { _ID=ID; }
-void User::setFirstName(std::string first_name) { _first_name=first_name; }
-void User::setLastName(std::string last_name) { _last_name=last_name; }
-void User::setAdress(std::string adress) { _adress=adress; }
-void User::setPhone(std::string phone) { _phone=phone; }
+void User::setFirstName(const std::string& first_name) { _first_name=first_name; }
+void User::setLastName(const std::string& last_name) { _last_name=last_name; }
+void User::setAdress(const std::string& adress) { _adress=adress; }
+void User::setPhone(const std::string& phone) { _phone=phone; }
 
-bool User::operator<(User& other)  {return _ID<other._ID;}
-bool User::operator<=(User& other) {return _ID<=other._ID;}
-bool User::operator>(User& other)  {return _ID>other._ID;}
-bool User::operator>=(User& other) {return _ID>=other._ID;}
-bool User::operator==(User& other) {return _ID==other._ID;}
-bool User::operator!=(User& other) {return _ID!=other._ID;}
+bool User::operator<(const User& other)  const {return (_ID<other._ID);}
+bool User::operator<=(const User& other) const {return (_ID<=other._ID);}
+bool User::operator>(const User& other)  const {return (_ID>other._ID);}
+bool User::operator>=(const User& other) const {return (_ID>=other._ID);}
+bool User::operator==(const User& other) const {return (_ID==other._ID);}
+bool User::operator!=(const User& other) const {return (_ID!=other._ID);}
 
 std::ostream& operator<<(std::ostream& out,const User& user)
 {
-	out<<user._ID<<" "<<user._first_name<<" "<<user._last_name<<std::endl;
+	if(out==cout)
+		out<<"First name: "<<user._first_name<<"\nLast name: "<<user._last_name<<std::endl;
+	else
+		out<<user._first_name<<","<<user._last_name;
 	return out;	
 }
 
-bool User::byName(User& other) {return _first_name<other._first_name;}
-bool User::bySurname(User& other){return _last_name<other._last_name;}
+bool User::byName(const User& other) const {return (_first_name<other._first_name);}
+bool User::bySurname(const User& other) const {return (_last_name<other._last_name);}
 
-bool User::byNameAndSurname(User& other)
+bool User::byNameAndSurname(const User& other) const
 {
-	return (_first_name+_last_name)<(other._first_name+other._last_name);
+	return ((_first_name+_last_name)<(other._first_name+other._last_name));
 }
