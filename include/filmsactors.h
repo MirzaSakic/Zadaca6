f3a4e8991f449d870cabf386b88e743c238e045a
MenisaHrvic <menisa.hrvic@hotmail.com>
2015-06-20 09:44:46 +0200

    20.062015.<Films-actors table created>

--- include/filmsactors.h
+++ include/filmsactors.h
@@ -12,19 +12,15 @@ class FilmsActors : public Table<FilmActor>
     FilmsActors(BinaryTree<FilmActor>&& data) : Table<FilmActor>(std::move(data)) {AddIndexOnActors();}
     FilmsActors(const std::string& file_path) : Table<FilmActor>(file_path) {_data.setLessThan(lte); }
 
-    FilmActor& GetBorrowedRecord(int,int);
-    sp::vector<FilmActor*> GetBorrowedRecords(int,int);
+    FilmActor& GetRecord(int,int);
     sp::vector<FilmActor*> GetRecordsForActor(int);
     sp::vector<FilmActor*> GetRecordsForFilm(int);
-    sp::vector<FilmActor*> GetBorrowedRecordsForActor(int);
-    sp::vector<FilmActor*> GetBorrowedRecordsForFilm(int);
 
-    const FilmActor& GetBorrowedRecord(int,int) const;
+
+    const FilmActor& GetRecord(int,int) const;
     const sp::vector<const FilmActor*> GetBorrowedRecords(int,int) const;
     const sp::vector<const FilmActor*> GetRecordsForActor(int) const;
     const sp::vector<const FilmActor*> GetRecordsForFilm(int) const;
-    const sp::vector<const FilmActor*> GetBorrowedRecordsForActor(int) const;
-    const sp::vector<const FilmActor*> GetBorrowedRecordsForFilm(int) const;
 
     void LoadFromFile();
     void AddElement(FilmActor&);
